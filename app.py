af6bbc6b4f3c378522ccbcb0893b5d7d2bf60f96
Shailja <s7thakur@uwaterloo.ca>
2023-10-18 18:03:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import tweepy
 app = Flask(__name__)
 
 # Set your Twitter API credentials
-consumer_key = 'WDuIdMfpfW4x3VYl0Z9PjUIXy'
-consumer_secret = 'da0jRyadIreruC8EJl7WlbVZl6Oi512FVvoEVCCAY9QzGtfK9u'
-access_token = '1534450153-e5kqD6i9GCKUZVH9TI894dMmyXOLAWJ1MebH34b'
-access_token_secret = 'OEB2R6GIqXJn9vw4TWIdJPemIzjMnlvEp5GDU43ZDQgT0'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
